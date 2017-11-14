b71214782ea42c16cb415ad05c6592ad03a4ada3
cxc8170 <cxc8170@rit.edu>
2017-11-13 19:52:59 -0500

    Mapped CTRL+C to graceful shutdown

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,4 @@
+extern crate ctrlc;
 extern crate hello;
 use hello::ThreadPool;
 use std::io::prelude::*;
@@ -6,10 +7,13 @@ use std::net::TcpStream;
 use std::fs::File;
 use std::thread;
 use std::time::Duration;
+use std::sync::{Mutex, Arc};
+use std::process;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     let pool = ThreadPool::new(4);
+    let m = Arc::new(Mutex::new(true));
 
     //Test code
     //let mut counter = 0;
@@ -29,13 +33,28 @@ fn main() {
     //    });
     //}
 
+    let arc = m.clone();
+    ctrlc::set_handler(move || { 
+        let mut value = arc.lock().unwrap();
+        println!("Shutting down.");
+        *value = false;
+    }).expect("Error setting Ctrl+C handler");
+
+    
     for stream in listener.incoming() {
+        let a = m.clone();
+        let value = a.lock().unwrap();
+        if *value == false{
+            break;
+        }
         let stream = stream.unwrap();
-        
+
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+    drop(pool);
+    process::exit(0);
 }
 
 fn handle_connection(mut stream: TcpStream) {
